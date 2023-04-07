f7bb74f4b2642cbcc8c9ed91777498ea8286eae7
MrLad01 <ladayodeji@gmail.com>
2023-04-07 16:41:09 +0100

    update

--- swift-learning/frontend/src/components/Explore.js
+++ swift-learning/frontend/src/components/Explore.js
@@ -1,7 +1,19 @@
+import { Link, NavLink } from "react-router-dom"
+
 export default function Explore(){
     return (
         <div>
-        
+            <h3>Explore most popular course</h3>
+            <div>
+                <NavLink>All</NavLink>
+                <NavLink>Web Development</NavLink>
+                <NavLink>UI/UX</NavLink>
+                <NavLink>Data Science</NavLink>
+                <NavLink>Graphics Design</NavLink>
+                <NavLink>Mobile Development</NavLink>
+            </div>
+            <div>Slide</div>
+            <Link>See more</Link>
         </div>
     )
 }
