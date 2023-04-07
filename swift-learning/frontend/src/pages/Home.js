ca2a01111bec689e023191692fe408d71d520a23
MrLad01 <ladayodeji@gmail.com>
2023-04-07 16:36:44 +0100

    update

--- swift-learning/frontend/src/pages/Home.js
+++ swift-learning/frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import Menu from "../components/Menu"
 import Intro from "../components/Intro"
+import Explore from "../components/Explore"
 
 export default function Home(){
     return (
         <div>
             <Menu />
             <Intro />
+            <Explore />
         </div>
     )
 }
