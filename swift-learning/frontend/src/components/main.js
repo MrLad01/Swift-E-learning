5c6dcb94b439d32c9e689a1b6b7243c3b2b6f046
MrLad01 <ladayodeji@gmail.com>
2023-04-07 16:15:06 +0100

    update

---
+++ swift-learning/frontend/src/components/main.js
@@ -0,0 +1,17 @@
+export default function main(){
+    return (
+        <div>
+            <p>
+                Gain knowledge from <span>expert instructors</span> at your own pace, anytime and anywhere
+            </p>
+            <p>
+                Unlock your potential with our engaging e-learning platform. Access high-quality courses anytime, anywhere and achieve your personal and professional goals with ease.
+            </p>
+            <button>Start Learning</button>
+            <div>Panaroma</div>
+            <div>
+                logos
+            </div>
+        </div>
+    )
+}
