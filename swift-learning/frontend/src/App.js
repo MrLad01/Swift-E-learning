6a08051d707796531b4da9b5068087c09e3c462f
MrLad01 <ladayodeji@gmail.com>
2023-04-07 13:56:38 +0100

    update

--- swift-learning/frontend/src/App.js
+++ swift-learning/frontend/src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-
+      <Route path="/" element={<Home />} />
     </Route>
   )
 )
