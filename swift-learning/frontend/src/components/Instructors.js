219b6ed15f6e1ecc9b953fca7905201218c9c7c7
MrLad01 <ladayodeji@gmail.com>
2023-04-08 22:21:57 +0100

    update

--- swift-learning/frontend/src/components/Instructors.js
+++ swift-learning/frontend/src/components/Instructors.js
@@ -2,6 +2,7 @@ export default function Instructors(){
     return (
         <div>
             <h3>Meet Our Instructors</h3>
+            <p>Benefitting from the expertise of the most accomplished professionala in your field.</p>
         </div>
     )
 }
