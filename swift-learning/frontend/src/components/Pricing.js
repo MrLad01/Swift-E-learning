7960a4d7a3125ef6d550a72db598144650241530
MrLad01 <ladayodeji@gmail.com>
2023-04-08 22:31:16 +0100

    update

--- swift-learning/frontend/src/components/Pricing.js
+++ swift-learning/frontend/src/components/Pricing.js
@@ -3,6 +3,7 @@ export default function Pricing(){
         <div>
             <h3>Our Pricing</h3>
             <p>Choose between pre-recorded courses or interactive classes led by an instructor in real-time.</p>
+            <p>card swiper</p>
         </div>
     )
 }
