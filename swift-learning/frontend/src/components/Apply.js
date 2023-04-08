a0ca75c4c1552a6db2f653b4439136793017098c
MrLad01 <ladayodeji@gmail.com>
2023-04-08 22:14:52 +0100

    update

--- swift-learning/frontend/src/components/Apply.js
+++ swift-learning/frontend/src/components/Apply.js
@@ -4,6 +4,7 @@ export default function Apply() {
             <div>
                 <h3>Transform yourself by learning at your convenience</h3>
                 <p>Our goal is not only provide students with education amd experiences that prepare them for a successful career, but also to support them on achieving success in their chosen field by encouraging them to explore their passions and become leaders in their field.</p>
+                <p>Apllications are now open</p>
             </div>
         </div>
     )
