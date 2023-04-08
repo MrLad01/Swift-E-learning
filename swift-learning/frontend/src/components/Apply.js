b4a7a39ccf39acf353a990745ccc7da5db295eda
MrLad01 <ladayodeji@gmail.com>
2023-04-08 22:10:46 +0100

    update

--- swift-learning/frontend/src/components/Apply.js
+++ swift-learning/frontend/src/components/Apply.js
@@ -1,5 +1,9 @@
 export default function Apply() {
     return (
-        <div></div>
+        <div>
+            <div>
+                <h3>Transform yourself by learning at your convenience</h3>
+            </div>
+        </div>
     )
 }
