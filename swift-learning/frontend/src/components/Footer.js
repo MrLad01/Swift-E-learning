b9a51d5fdea98bd3c28dae8c4abd436ee781a434
MrLad01 <ladayodeji@gmail.com>
2023-04-09 14:21:43 +0100

    update

--- swift-learning/frontend/src/components/Footer.js
+++ swift-learning/frontend/src/components/Footer.js
@@ -1,5 +1,16 @@
 export default function Footer(){
     return (
-        <div></div>
+        <div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <h4>Swift</h4>
+                        <p>e-Learning</p>
+                    </div>
+                </div>
+                <p>Most affordable and friendly E-Learning Platform </p>
+            </div>
+        </div>
     )
 }
