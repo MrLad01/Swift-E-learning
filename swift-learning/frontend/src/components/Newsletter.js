aa3c27fb367f27d8d176e8d8de72af6ee2158665
MrLad01 <ladayodeji@gmail.com>
2023-04-09 14:12:13 +0100

    update

--- swift-learning/frontend/src/components/Newsletter.js
+++ swift-learning/frontend/src/components/Newsletter.js
@@ -2,6 +2,7 @@ export default function Newsletter(){
     return (
         <div>
             <h3>Subscribe to our Newsletter</h3>
+            <p>Our newsletter is the best way to stay up-to-date with everything that's going on our new and updated courses.</p>
         </div>
     )
 } 
