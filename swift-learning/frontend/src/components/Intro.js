c6cc0cdbfc425e895f1217a4999c7871ca45dfb0
MrLad01 <ladayodeji@gmail.com>
2023-04-12 06:28:54 +0100

    update

--- swift-learning/frontend/src/components/Intro.js
+++ swift-learning/frontend/src/components/Intro.js
@@ -1,10 +1,10 @@
 export default function Intro(){
     return (
         <div className="first-page">
-            <p>
+            <p className="first-paragraph">
                 Gain knowledge from <span>expert instructors</span> at your own pace, anytime and anywhere
             </p>
-            <p>
+            <p className="second-paragraph">
                 Unlock your potential with our engaging e-learning platform. Access high-quality courses anytime, anywhere and achieve your personal and professional goals with ease.
             </p>
             <button>Start Learning</button>
