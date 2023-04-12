0c5dc9b674e615d342eb98945ae1ebad9af02d4b
MrLad01 <ladayodeji@gmail.com>
2023-04-12 07:29:01 +0100

    update

--- swift-learning/frontend/data/data.js
+++ swift-learning/frontend/data/data.js
@@ -3,3 +3,29 @@ import img2 from "../data/images/Frame 1000001063img2.png"
 import img3 from "../data/images/Frame 1000001063img3.png"
 import img4 from "../data/images/Frame 1000001063img4.png"
 import img5 from "../data/images/Frame 1000001063img5.png"
+
+
+const data =[
+    {
+        id:1,
+        image: img1
+    },
+    {
+        id:2,
+        image: img2
+    },
+    {
+        id:3,
+        image: img3
+    },
+    {
+        id:4,
+        image: img4
+    },
+    {
+        id:5,
+        image: img5
+    }
+]
+
+export default data
