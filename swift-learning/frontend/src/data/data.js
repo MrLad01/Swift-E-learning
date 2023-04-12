120fed1cefc6df36653be59aee6ad844c38cf862
MrLad01 <ladayodeji@gmail.com>
2023-04-12 08:28:45 +0100

    update

--- swift-learning/frontend/src/data/data.js
+++ swift-learning/frontend/src/data/data.js
@@ -9,27 +9,32 @@ const data =[
     {
         id:1,
         image: img1,
-        img: "../data/images/Frame 1000001063img1.png"
+        img: "../../data/images/Frame 1000001063img1.png",
+        alt: "loading..."
     },
     {
         id:2,
         image: img2,
-        img: "../data/images/Frame 1000001064img2.png"
+        img: "../../data/images/Frame 1000001064img2.png",
+        alt: "loading..."
     },
     {
         id:3,
         image: img3,
-        img: "../data/images/Frame 1000001065img3.png"
+        img: "../../data/images/Frame 1000001065img3.png",
+        alt: "loading..."
     },
     {
         id:4,
         image: img4,
-        img: "../data/images/Frame 1000001066img4.png"
+        img: "../../data/images/Frame 1000001066img4.png",
+        alt: "loading..."
     },
     {
         id:5,
         image: img5,
-        img: "../data/images/Frame 1000001067img5.png"
+        img: "../../data/images/Frame 1000001067img5.png",
+        alt: "loading..."
     }
 ]
 
