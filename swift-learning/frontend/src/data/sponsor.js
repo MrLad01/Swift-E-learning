25fb3be50a2d9924acbfb6f62090b9c94d3ff632
MrLad01 <ladayodeji@gmail.com>
2023-04-13 23:29:25 +0100

    update

---
+++ swift-learning/frontend/src/data/sponsor.js
@@ -0,0 +1,39 @@
+import img1 from "../data/images/Frame 1000001063img1.png"
+import img2 from "../data/images/Frame 1000001064img2.png"
+import img3 from "../data/images/Frame 1000001065img3.png"
+import img4 from "../data/images/Frame 1000001066img4.png"
+import img5 from "../data/images/Frame 1000001067img5.png"
+
+
+const Pictures = [
+    {
+        id:1,
+        image: img1,
+        alt: "loading..."
+    },
+    {
+        id:2,
+        image: img2,
+        alt: "loading..."
+    },
+    {
+        id:3,
+        image: img3,
+        alt: "loading..."
+    },
+    {
+        id:4,
+        image: img4,
+        alt: "loading..."
+    },
+    {
+        id:5,
+        image: img5,
+        alt: "loading..."
+    },
+    {
+        id:5,
+        image: img5,
+        alt: "loading..."
+    }
+]
