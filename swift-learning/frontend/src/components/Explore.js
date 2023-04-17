0b456603baf22266c91cf7ce30f16e14c528bc88
MrLad01 <ladayodeji@gmail.com>
2023-04-17 13:26:39 +0100

    update

--- swift-learning/frontend/src/components/Explore.js
+++ swift-learning/frontend/src/components/Explore.js
@@ -6,10 +6,15 @@ export default function Explore(){
             <h3 className="explore-head">Explore most popular course</h3>
             <div className="explore-box">
                 <NavLink>All</NavLink>
+                <div className="vertical-line"></div>
                 <NavLink>Web Development</NavLink>
+                <div className="vertical-line"></div>
                 <NavLink>UI/UX</NavLink>
+                <div className="vertical-line"></div>
                 <NavLink>Data Science</NavLink>
+                <div className="vertical-line"></div>
                 <NavLink>Graphics Design</NavLink>
+                <div className="vertical-line"></div>
                 <NavLink>Mobile Development</NavLink>
             </div>
             <div>Slide</div>
