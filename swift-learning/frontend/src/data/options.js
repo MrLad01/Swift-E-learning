a2088de4f8b37d2a0603779bf3b3f153d59fd0d6
MrLad01 <ladayodeji@gmail.com>
2023-04-17 13:46:07 +0100

    update

--- swift-learning/frontend/src/data/options.js
+++ swift-learning/frontend/src/data/options.js
@@ -6,6 +6,14 @@ const Options = [
         head: "Frontend Web Development With ReactJS",
         context: " Hello guys, welcome to front-end development with React!! This couse was made specifically for you, to take you...",
         time: 55
+    },
+    {
+        id: 2, 
+        image: img.png,
+        alt: "loading...",
+        head: "Introduction to UI/UX Design",
+        context: " Hello guys, welcome to front-end development with React!! This couse was made specifically for you, to take you...",
+        time: 55
     }
 ]
 export default Options
