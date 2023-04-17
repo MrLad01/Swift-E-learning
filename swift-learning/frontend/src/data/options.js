d631cf2d324bed4d49cfee69d836a112babf3bb8
MrLad01 <ladayodeji@gmail.com>
2023-04-17 15:15:57 +0100

    update

--- swift-learning/frontend/src/data/options.js
+++ swift-learning/frontend/src/data/options.js
@@ -1,7 +1,12 @@
+import image1 from "../data/images/Frame.png"
+import image2 from "../data/images/IMAGE.png"
+import image3 from "../data/images/IMAGE (1).png"
+import image4 from "../data/images/IMAGE (2).png"
+
 const options = [
     {
         id: 1, 
-        image: img.png,
+        image: image1,
         alt: "loading...",
         head: "Frontend Web Development With ReactJS",
         context: " Hello guys, welcome to front-end development with React!! This couse was made specifically for you, to take you...",
@@ -9,7 +14,7 @@ const options = [
     },
     {
         id: 2, 
-        image: img.png,
+        image: image2,
         alt: "loading...",
         head: "Introduction to UI/UX Design",
         context: "In this short course, you will learn the basics of UI/UX Design, including Design principles, color theory, typography, and layout. You will also learn how to...",
@@ -17,7 +22,7 @@ const options = [
     },
     {
         id: 3, 
-        image: img.png,
+        image: image3,
         alt: "loading...",
         head: "Web Development (ReactJS)",
         context: "ReactJS is a popular JavaScript library for building user interfaces. In this short course, you will learn the basics of ReactJS, including components, state, props, and lifecycle methods... ",
@@ -25,7 +30,7 @@ const options = [
     },
     {
         id: 4, 
-        image: img.png,
+        image: image4,
         alt: "loading...",
         head: "JavaScript Basics",
         context: "JavaScript(JS) is a high-level programming language that is commonly used for creating web pages and web applications",
