90fa972c21b203aa4f6805b6e6b40ae849dc8942
MrLad01 <ladayodeji@gmail.com>
2023-04-17 13:45:18 +0100

    update

---
+++ swift-learning/frontend/src/data/options.js
@@ -0,0 +1,11 @@
+const Options = [
+    {
+        id: 1, 
+        image: img.png,
+        alt: "loading...",
+        head: "Frontend Web Development With ReactJS",
+        context: " Hello guys, welcome to front-end development with React!! This couse was made specifically for you, to take you...",
+        time: 55
+    }
+]
+export default Options
