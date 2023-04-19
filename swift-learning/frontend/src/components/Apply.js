6cf3cde8d38c3daa141053cbacbd113db955bd7f
MrLad01 <ladayodeji@gmail.com>
2023-04-19 12:22:55 +0100

    update

--- swift-learning/frontend/src/components/Apply.js
+++ swift-learning/frontend/src/components/Apply.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import pics from "../data/images/IMAGEsee.png"
 
 export default function Apply() {
     return (
@@ -7,8 +8,9 @@ export default function Apply() {
                 <h3>Transform yourself by learning at your convenience</h3>
                 <p>Our goal is not only provide students with education amd experiences that prepare them for a successful career, but also to support them on achieving success in their chosen field by encouraging them to explore their passions and become leaders in their field.</p>
                 <p>Apllications are now open</p>
+                <Link><button>Apply Now</button></Link>
             </div>
-            <Link><button>Apply Now</button></Link>
+            <img src={pics} alt="Loading..."/>
         </div>
     )
 }
