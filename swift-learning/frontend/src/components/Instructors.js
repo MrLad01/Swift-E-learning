2f24e7e5366ef385fbc32e39af951b31cabd91df
MrLad01 <ladayodeji@gmail.com>
2023-04-20 22:32:04 +0100

    update

--- swift-learning/frontend/src/components/Instructors.js
+++ swift-learning/frontend/src/components/Instructors.js
@@ -9,6 +9,12 @@ export default function Instructors(){
                 <div className="instructors-circle3"></div>
                 <div className="instructors-circle4"></div>
                 <div className="instructors-circle5"></div>
+                <div className="instructors-circle6"></div>
+                <div className="instructors-circle7"></div>
+                <div className="instructors-circle8"></div>
+                <div className="instructors-circle9"></div>
+                <div className="instructors-circle10"></div>
+                <div className="instructors-circle11"></div>
             </div>
             <h4>Swiper</h4>
         </div>
