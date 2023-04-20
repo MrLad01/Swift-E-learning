c4434b53d0762c8930041a6b14ddb1072ee502bc
MrLad01 <ladayodeji@gmail.com>
2023-04-21 00:28:52 +0100

    update

--- swift-learning/frontend/src/components/Instructors.js
+++ swift-learning/frontend/src/components/Instructors.js
@@ -4,17 +4,17 @@ export default function Instructors(){
             <h3 className="instructors-head">Meet Our Instructors</h3>
             <p className="instructors-text">Benefitting from the expertise of the most accomplished professionala in your field.</p>
             <div className="instructors-background">
-                <div className="instructors-circle1"></div>
-                <div className="instructors-circle2"></div>
-                <div className="instructors-circle3"></div>
-                <div className="instructors-circle4"></div>
-                <div className="instructors-circle5"></div>
-                <div className="instructors-circle6"></div>
-                <div className="instructors-circle7"></div>
-                <div className="instructors-circle8"></div>
-                <div className="instructors-circle9"></div>
-                <div className="instructors-circle10"></div>
-                <div className="instructors-circle11"></div>
+                <div className="instructors-circle circle1"></div>
+                <div className="instructors-circle circle2"></div>
+                <div className="instructors-circle circle3"></div>
+                <div className="instructors-circle circle4"></div>
+                <div className="instructors-circle circle5"></div>
+                <div className="instructors-circle circle6"></div>
+                <div className="instructors-circle circle7"></div>
+                <div className="instructors-circle circle8"></div>
+                <div className="instructors-circle circle9"></div>
+                <div className="instructors-circle circle10"></div>
+                <div className="instructors-circle circle11"></div>
             </div>
             <h4>Swiper</h4>
         </div>
