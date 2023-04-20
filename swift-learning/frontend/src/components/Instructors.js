91e13509230deb5b7fc3305efc7516f1eae72108
MrLad01 <ladayodeji@gmail.com>
2023-04-20 22:27:42 +0100

    update

--- swift-learning/frontend/src/components/Instructors.js
+++ swift-learning/frontend/src/components/Instructors.js
@@ -3,6 +3,7 @@ export default function Instructors(){
         <div className="instructors">
             <h3 className="instructors-head">Meet Our Instructors</h3>
             <p className="instructors-text">Benefitting from the expertise of the most accomplished professionala in your field.</p>
+            <div className="instructors-background"></div>
             <h4>Swiper</h4>
         </div>
     )
