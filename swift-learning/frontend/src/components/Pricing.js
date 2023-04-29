c21c10afb0ba4130050985fdfdf4c12187351f5d
MrLad01 <ladayodeji@gmail.com>
2023-04-29 13:26:56 +0100

    update

--- swift-learning/frontend/src/components/Pricing.js
+++ swift-learning/frontend/src/components/Pricing.js
@@ -1,8 +1,8 @@
 export default function Pricing(){
     return (
-        <div>
-            <h3>Our Pricing</h3>
-            <p>Choose between pre-recorded courses or interactive classes led by an instructor in real-time.</p>
+        <div className="Pricing">
+            <h3 className="p-header">Our Pricing</h3>
+            <p className="p-subheader">Choose between pre-recorded courses or interactive classes led by an instructor in real-time.</p>
             <p>card swiper</p>
         </div>
     )
