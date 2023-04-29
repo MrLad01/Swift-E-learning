01e8b200d66f04fe09bcf1f2e98f6707548b5318
MrLad01 <ladayodeji@gmail.com>
2023-04-29 14:00:51 +0100

    update

--- swift-learning/frontend/src/components/Testimonials.js
+++ swift-learning/frontend/src/components/Testimonials.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function Testimonials(){
     return (
-        <div>
-            <h3>Testimonials</h3>
-            <p>What they say about us</p>
+        <div className="testimonials">
+            <h3 className="t-header">Testimonials</h3>
+            <p className="t-subheader">What they say about us</p>
             <p>Cards swiper</p>
             <Link><button>Apply Now</button></Link>
         </div>
