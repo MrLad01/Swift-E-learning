23eea8342d91c145e90afac4c048cdf8aaae3611
MrLad01 <ladayodeji@gmail.com>
2023-05-18 17:17:36 +0100

    update

--- swift-learning/frontend/src/components/Footer.js
+++ swift-learning/frontend/src/components/Footer.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function Footer(){
     return (
-        <div>
-            <div>
+        <div className="footer">
+            <div className="f-logo">
                 <div>
                     <img src="" alt="" />
                     <div>
@@ -13,36 +13,38 @@ export default function Footer(){
                 </div>
                 <p>Most affordable and friendly E-Learning Platform </p>
             </div>
-            <div>
-                <div>
-                    <h4>Quick Links</h4>
-                    <Link>Blog</Link>
-                    <Link>Pricing</Link>
-                    <Link>Instructors</Link>
-                    <Link>Careers</Link>
-                    <Link>Support</Link>
-                    <Link>Terms & Condition</Link>
-                    <Link>Privacy Policies</Link>
+            <div className="f-middle">
+                <div classsName="f-links">
+                    <div>
+                        <h4>Quick Links</h4>
+                        <Link>Blog</Link>
+                        <Link>Pricing</Link>
+                        <Link>Instructors</Link>
+                        <Link>Careers</Link>
+                        <Link>Support</Link>
+                        <Link>Terms & Condition</Link>
+                        <Link>Privacy Policies</Link>
+                    </div>
+                    <div>
+                        <h4>Learning Options</h4>
+                        <Link>Self-paced Courses</Link>
+                        <Link>Incubator Programs</Link>
+                        <Link>Personal Coaching</Link>
+                        <Link>Enterprise Learning</Link>
+                        <Link>Career-building Paths</Link>
+                    </div>
+                    <div>
+                        <h4>Courses</h4>
+                        <Link>JavaScript</Link>
+                        <Link>React</Link>
+                        <Link>Power BI</Link>
+                        <Link>Python</Link>
+                        <Link>SQL</Link>
+                    </div>
                 </div>
-                <div>
-                    <h4>Learning Options</h4>
-                    <Link>Self-paced Courses</Link>
-                    <Link>Incubator Programs</Link>
-                    <Link>Personal Coaching</Link>
-                    <Link>Enterprise Learning</Link>
-                    <Link>Career-building Paths</Link>
-                </div>
-                <div>
-                    <h4>Courses</h4>
-                    <Link>JavaScript</Link>
-                    <Link>React</Link>
-                    <Link>Power BI</Link>
-                    <Link>Python</Link>
-                    <Link>SQL</Link>
-                </div>
+                <p>&copy; Copyright 2023 All Rights Reserved </p>
             </div>
-            <p>&copy; Copyright 2023 All Rights Reserved </p>
-            <div>
+            <div className="f-visit">
                 <p>Visit Us</p>
                 <div>
                     <div></div>
