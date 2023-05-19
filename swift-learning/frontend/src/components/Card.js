60c3568907d5d4b521a0be60b8908cbd6cd0a4f4
MrLad01 <ladayodeji@gmail.com>
2023-05-19 17:51:43 +0100

    update

--- swift-learning/frontend/src/components/Card.js
+++ swift-learning/frontend/src/components/Card.js
@@ -1,7 +1,15 @@
-export default function Card() {
+export default function Card({courses}) {
   return (
     <div>
-      
+      {
+        courses.map((course) => (
+            <>
+                <img src={course.img} alt={course.alt} />
+                <h3>{course.head}</h3>
+                <h4>{courses.amount}</h4>
+            </>
+        ))
+      }
     </div>
   )
 }
