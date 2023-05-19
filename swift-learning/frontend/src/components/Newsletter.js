e1805251f8dacf5822bbf5f6edb6d1e4243b4393
MrLad01 <ladayodeji@gmail.com>
2023-05-19 15:41:11 +0100

    update

--- swift-learning/frontend/src/components/Newsletter.js
+++ swift-learning/frontend/src/components/Newsletter.js
@@ -2,7 +2,7 @@ export default function Newsletter(){
     return (
         <div className="newsletter">
             <h3>Subscribe to our Newsletter</h3>
-            <p>Our newsletter is the best way to stay up-to-date with everything that's going on our new and updated courses.</p>
+            <p>Our newsletter is the best way to stay up-to-date with everything that's going<br /> on our new and updated courses.</p>
             <div id="newsletter-input">
                 <input type="text" placeholder="Type email here" />
                 <button>Subscribe</button>
