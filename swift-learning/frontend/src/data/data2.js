4fdd85d1560483328fc09ab8fbc253c8357efdae
MrLad01 <ladayodeji@gmail.com>
2023-05-19 17:24:46 +0100

    update

---
+++ swift-learning/frontend/src/data/data2.js
@@ -0,0 +1,36 @@
+const courses = [
+    {
+        id:1,
+        img: img1,
+        head:"Self-Paced Courses",
+        amount: "$14.99 /month",
+        list: [
+         "Access all Recorded courses",
+         "Certification"
+        ]
+    },
+    {
+        id: 2,
+        img: img2,
+        head: "Incubator Program",
+        amount: "$149.99 /Program",
+        list: [
+            "5-7 months intensive training",
+            "Skill-to-job program",
+            "3 months internship placements"
+        ]
+    },
+    {
+        id: 3,
+        img: img3,
+        head: "Personal Coaching",
+        amount: "$249.99 /Cohort",
+        list: [
+            "1:1 Coaching",
+            "Decide what to learn with the instructor",
+            "Flexible class time"
+        ]
+    }
+]
+
+export default courses;
