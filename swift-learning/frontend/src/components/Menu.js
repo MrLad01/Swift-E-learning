20f91993dbac222608a01346b50a99c9c44c8418
MrLad01 <ladayodeji@gmail.com>
2023-05-25 16:05:04 +0100

    update

--- swift-learning/frontend/src/components/Menu.js
+++ swift-learning/frontend/src/components/Menu.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Menu(){
     return (
-        <div className="header">
+        <nav className="header">
             <div className="logo">
                 <div className="logo-img">
                     <div className="logo-box box1"></div>
@@ -18,7 +18,7 @@ export default function Menu(){
             <div className="header-nav">
                 <Link>Home</Link>
                 <div>
-                    <select name="Courses" id="courses" value="Courses">
+                    {/* <select name="Courses" id="courses" value="Courses">
                         <option value="" selected>Courses</option>
                         <option value="ui/ux">
                             <Link>
@@ -65,7 +65,55 @@ export default function Menu(){
                                 </div>
                             </Link>
                         </option>
-                    </select>
+                    </select> */}
+                    <ul name="Courses" id="courses" value="Courses">
+                        <div value="" selected>Courses</div>
+                        <li value="ui/ux">
+                            <Link>
+                                <img src="" alt="" />
+                                <div>
+                                    <h3><strong>Introduction to UI/UX Design</strong></h3>
+                                    <h4>In this short course. you will learn the basics of UI/UX design, including design principles, color theory, typography, and layout. </h4>
+                                </div>
+                            </Link>
+                        </li>
+                        <li value="python">
+                            <Link>
+                                <img src="" alt="" />
+                                <div>
+                                    <h3><strong>Introduction to Python</strong></h3>
+                                    <h4>Python is a high-level programming language that is known for its simplicity and readability. </h4>
+                                </div>
+                            </Link>
+                        </li>
+                        <li value="react">
+                            <Link>
+                                <img src="" alt="" />
+                                <div>
+                                    <h3><strong>Frontend Web Development With ReactJS</strong></h3>
+                                    <h4>Hello guys, welcome to front-end web development with React!!. This course was made specifically for you, to take you f... </h4>
+                                </div>
+                            </Link>
+                        </li>
+                        <li value="wordpress">
+                            <Link>
+                                <img src="" alt="" />
+                                <div>
+                                    <h3><strong>Introduction to WordPress</strong></h3>
+                                    <h4>WordPress is a popular content management system (CMS) that alllows users to easily create, manage, and publish digital... </h4>
+                                </div>
+                            </Link>
+                        </li>
+                        <li value="js">
+                            <Link>
+                                <img src="" alt="" />
+                                <div>
+                                    <h3><strong>JavaScript Basics</strong></h3>
+                                    <h4>JavaScript(JS) is a high-level programming language that is commonly used for creating interactive web pages and... </h4>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
                 <Link to="#pricing">Pricing</Link>
                 <Link to="#testimonials">Testimonials</Link>
@@ -74,6 +122,6 @@ export default function Menu(){
                 <input type="text" placeholder="Search for course"/>
                 <button>Search</button>
             </div>
-        </div>
+        </nav>
     )
 }
