3c3f52ca30f176ec2e8c218be22b29b14b229092
MrLad01 <ladayodeji@gmail.com>
2023-05-31 11:48:45 +0100

    update

--- swift-learning/frontend/src/pages/SignUp.jsx
+++ swift-learning/frontend/src/pages/SignUp.jsx
@@ -3,30 +3,28 @@ import Side from "../components/Side"
 
 const SignUp = () => {
   return (
-    <div id="sside">
-        <>
-            <Side />
+    <>
+        <Side />
+        <div id="sside">
+        <Form>
+            <h2>Welcome to Swift e-Learning</h2>
+            <h5>Creating learning experiences to your doorstep!</h5>
+            <h6>Let us meet you</h6>
+            <input type="text" name="" id="" placeholder="Full name" />
+            <input type="text" name="" id="" placeholder="Course" />
+            <input type="text" name="" id="" placeholder="Email" />
             <div>
-            <Form>
-                <h2>Welcome to Swift e-Learning</h2>
-                <h5>Creating learning experiences to your doorstep!</h5>
-                <h6>Let us meet you</h6>
-                <input type="text" name="" id="" placeholder="Full name" />
-                <input type="text" name="" id="" placeholder="Course" />
-                <input type="text" name="" id="" placeholder="Email" />
-                <div>
-                    <input type="checkbox" name="" id="" />
-                    <p>Yes, I would like to recieve emails about feature updates, emerging partnerships, community information and so on.</p>
-                    <p>I agree to the <Link>Terms of Use</Link> and <Link>Privacy Policy</Link></p>
-                </div>
-                <button>Get Started</button>
-                <button>Sign In</button>
-                <p>or</p>
-                <button>Continue with Google</button>
-            </Form>
+                <input type="checkbox" name="" id="" />
+                <p>Yes, I would like to recieve emails about feature updates, emerging partnerships, community information and so on.</p>
+                <p>I agree to the <Link>Terms of Use</Link> and <Link>Privacy Policy</Link></p>
             </div>
-        </>
-    </div>
+            <button>Get Started</button>
+            <button>Sign In</button>
+            <p>or</p>
+            <button>Continue with Google</button>
+        </Form>
+        </div>
+    </>
   )
 }
 
